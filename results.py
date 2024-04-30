f352f0a3be8ccb6cfc6ae72e582745ed0009e9de
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2024-04-30 12:09:55 +0400

    feat: add metrics, graphs and results processing

--- results.py
+++ results.py
@@ -1,16 +1,54 @@
-class SectionData:
-    def __init__(self, timestamp, measurement, tags, fields):
-        self.timestamp = timestamp
-        self.measurement = measurement
-        self.tags = tags
-        self.fields = fields
+import argparse
+import json
+import os
 
-    def __repr__(self):
-        return f"SectionData(timestamp={self.timestamp}, measurement='{self.measurement}', tags={self.tags}, " \
-               f"fields={self.fields})"
+import utils
+import matplotlib.pyplot as plt
 
 
-def get_sections(text):
+# get_files will return the files in the following format:
+# {'warmup_results': 'file.txt', 'warmup_response': 'file.txt', results': ['file.txt'],
+# 'responses': ['file.txt']}
+def get_files(results_paths, client):
+    # Get all the files in the results folder that match the client
+    directory = os.listdir(results_paths)
+    files = {
+        'warmup_results': None,
+        'warmup_response': None,
+        'results': [],
+        'responses': [],
+    }
+    for file in directory:
+        if file.startswith(f'{client}_response'):
+            files['responses'].append(file)
+        elif file.startswith(f'{client}_results'):
+            files['results'].append(file)
+        elif file.startswith(f'warmup_{client}_response'):
+            files['warmup_response'] = file
+        elif file.startswith(f'warmup_{client}_results'):
+            files['warmup_results'] = file
+    return files
+
+
+def read_responses(text):
+    responses = []
+    for line in text.split('\n'):
+        try:
+            if line is None or line == '':
+                continue
+            data = json.loads(line)
+            if "result" in data and isinstance(data["result"], dict) and "payloadStatus" in data["result"]:
+                response = utils.PayloadResponse.from_dict(data)
+                responses.append(response)
+            else:
+                response = utils.RPCResponse.from_dict(data)
+                responses.append(response)
+        except json.JSONDecodeError as e:
+            print(f"Error parsing JSON: {e}")
+    return responses
+
+
+def read_results(text):
     sections = {}
     for sections_text in text.split('--------------------------------------------------------------'):
         timestamp = None
@@ -40,71 +78,91 @@ def get_sections(text):
                     fields[data[0]] = data[1]
 
         if timestamp is not None and measurement is not None:
-            sections[measurement] = SectionData(timestamp, measurement, tags, fields)
+            sections[measurement] = utils.SectionData(timestamp, measurement, tags, fields)
 
     return sections
 
 
-def process_results(results_folder):
+def extract_data_per_client(client, results_paths):
+    file_names = get_files(results_paths, client)
+    # Get the responses from the files
+    responses = {}
+    for response in file_names['responses']:
+        # parse the name to get the run number
+        run = response.split('.')[0].split('_')[2]
+        with open(f'{results_paths}/{response}', 'r') as file:
+            text = file.read()
+            responses[run] = read_responses(text)
+    # Get the results from the files
+    results = {}
+    for result in file_names['results']:
+        # parse the name to get the run number
+        run = result.split('.')[0].split('_')[2]
+        with open(f'{results_paths}/{result}', 'r') as file:
+            text = file.read()
+            results[run] = read_results(text)
+    # Get the warmup responses
+    with open(f'{results_paths}/{file_names["warmup_response"]}', 'r') as file:
+        text = file.read()
+        warmup_responses = read_responses(text)
+    # Get the warmup results
+    with open(f'{results_paths}/{file_names["warmup_results"]}', 'r') as file:
+        text = file.read()
+        warmup_results = read_results(text)
+    return responses, results, warmup_responses, warmup_results
 
-    # Read the results
-    # make an array of results based on
+
+# Print graphs and tables with the results
+def process_results(client_results, results_paths, method, field):
+    plt.figure(figsize=(10, 5))
+    for client, data in client_results.items():
+        results_max = []
+        for i in range(len(data['results'])):
+            results_max.append(float(data['results'][str(i)][method].fields[field]))
+
+        x = range(1, len(data['results']) + 1)
+        plt.plot(x, results_max, label=client)
+        plt.xticks(list(x)[::1])
+    plt.legend()
+    plt.title(f'{field} results')
+    plt.savefig(f'{results_paths}/{method}_{field}_results.png')
+    plt.close()
     pass
 
-# Example usage
 
 def main():
-    text = """\
-    # TIMESTAMP: 638497082586960109
-    # MEASUREMENT: [Application] Messages
-    # TAGS:
-                      mtype = counter
-                       unit = items
-    # FIELDS:
-                      value = 4
-    --------------------------------------------------------------
-    # TIMESTAMP: 638497082586960109
-    # MEASUREMENT: [Application] Succeeded
-    # TAGS:
-                      mtype = counter
-                       unit = items
-    # FIELDS:
-                      value = 4
-    --------------------------------------------------------------
-    # TIMESTAMP: 638497082586960109
-    # MEASUREMENT: [Application] engine_forkchoiceUpdatedV3
-    # TAGS:
-                      mtype = timer
-                       unit = req
-                   unit_dur = ms
-                  unit_rate = ms
-    # FIELDS:
-                count.meter = 2
-                     rate1m = 0
-                     rate5m = 0
-                    rate15m = 0
-                  rate.mean = 0.005619814891839582
-                    samples = 2
-                       last = 8.879839
-                 count.hist = 2
-                        sum = 9.150521
-                        min = 0.270682
-                        max = 8.879839
-                       mean = 4.5752605
-                     median = 4.5752605
-                     stddev = 6.087593294999634
-                       p999 = 8.879839
-                        p99 = 8.879839
-                        p98 = 8.879839
-                        p95 = 8.879839
-                        p75 = 8.879839
-    """
+    parser = argparse.ArgumentParser(description='Benchmark script')
+    parser.add_argument('--resultsPath', type=str, help='Path to gather the results', default='results')
+    parser.add_argument('--clients', type=str, help='Client we want to gather the metrics, if you want to compare, '
+                                                    'split them by comma, ex: nethermind,geth', default='nethermind')
 
-    sections = get_sections(text)
+    # Parse command-line arguments
+    args = parser.parse_args()
 
-    for section in sections:
-        print(section)
-        print('---')
+    # Get client name and test case folder from command-line arguments
+    results_paths = args.resultsPath
+    clients = args.clients
+
+    # Get the computer spec
+    with open(os.path.join(results_paths, 'computer_specs.txt'), 'r') as file:
+        text = file.read()
+        computer_spec = text
+
+    client_results = {}
+    for client in clients.split(','):
+        responses, results, warmup_responses, warmup_results = extract_data_per_client(client, results_paths)
+        client_results[client] = {
+            'responses': responses,
+            'results': results,
+            'warmup_responses': warmup_responses,
+            'warmup_results': warmup_results
+        }
+
+    print(computer_spec)
+
+    for method in ['engine_forkchoiceUpdatedV3', 'engine_newPayloadV3']:
+        for field in ['max', 'min', 'mean', 'sum']:
+            process_results(client_results, results_paths, method, field)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -3,6 +3,62 @@ import platform
 import psutil
 
 
+class SectionData:
+    def __init__(self, timestamp, measurement, tags, fields):
+        self.timestamp = timestamp
+        self.measurement = measurement
+        self.tags = tags
+        self.fields = fields
+
+    def __repr__(self):
+        return f"SectionData(timestamp={self.timestamp}, measurement='{self.measurement}', tags={self.tags}, " \
+               f"fields={self.fields})"
+
+
+class RPCResponse:
+    def __init__(self, jsonrpc, result, id):
+        self.jsonrpc = jsonrpc
+        self.result = result
+        self.id = id
+
+    def __repr__(self):
+        return f"RPCResponse(jsonrpc={self.jsonrpc}, result={self.result}, id={self.id})"
+
+    @staticmethod
+    def from_dict(data):
+        jsonrpc = data.get("jsonrpc")
+        result = data.get("result")
+        id = data.get("id")
+        return RPCResponse(jsonrpc, result, id)
+
+    def get_result_status(self):
+        if self.result and "status" in self.result:
+            return self.result["status"]
+        return None
+
+
+class PayloadResponse:
+    def __init__(self, jsonrpc, result, id):
+        self.jsonrpc = jsonrpc
+        self.result = result
+        self.id = id
+
+    def __repr__(self):
+        return f"PayloadResponse(jsonrpc={self.jsonrpc}, result={self.result}, id={self.id})"
+
+    @staticmethod
+    def from_dict(data):
+        jsonrpc = data.get("jsonrpc")
+        result = data.get("result")
+        id = data.get("id")
+        return PayloadResponse(jsonrpc, result, id)
+
+    def get_payload_status(self):
+        if self.result and "payloadStatus" in self.result and "status" in self.result["payloadStatus"]:
+            return self.result["payloadStatus"]["status"]
+        return None
+
+
 def print_computer_specs():
     info = "Computer Specs:\n"
     cpu = cpuinfo.get_cpu_info()
