e29a12c3d3b63136bdb12d354d5a2dbc87f224e8
Kamil Chodoła <43241881+kamilchodola@users.noreply.github.com>
2025-06-11 00:41:26 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -50,10 +50,10 @@ for run in $(seq 1 $RUNS); do
       for run in $(seq 1 10); do
         if [ -z "$WARMUP_FILE" ]; then
           echo "Running script without warm up."
-          python3 run_kute.py --output WARMresults --testsPath "$test_dir/Origin_150M.txt" --jwtPath /tmp/jwtsecret --client $client --run $run
+          python3 run_kute.py --output WARMresults --testsPath "warmup-tests/Origin_150M.txt" --jwtPath /tmp/jwtsecret --client $client --run $run
         else
           echo "Using provided warm up file: $WARMUP_FILE"
-          python3 run_kute.py --output WARMresults --testsPath "$test_dir/Origin_150M.txt" --jwtPath /tmp/jwtsecret --warmupPath "$WARMUP_FILE" --client $client --run $run
+          python3 run_kute.py --output WARMresults --testsPath "warmup-tests/Origin_150M.txt" --jwtPath /tmp/jwtsecret --warmupPath "$WARMUP_FILE" --client $client --run $run
         fi
       done
       if [ -z "$WARMUP_FILE" ]; then
