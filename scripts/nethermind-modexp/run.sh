346e5b43feba45b79c109bbb74d47e3a20d91363
Kamil Chodola <kamil.chodola@gmail.com>
2025-06-18 12:26:26 +0000

    add nethermind modexp dir

---
+++ scripts/nethermind-modexp/run.sh
@@ -0,0 +1,11 @@
+# Prepare nethermind image that we will use on the script
+cd scripts/nethermind
+
+cp chainspec.json /tmp/chainspec.json
+cp jwtsecret /tmp/jwtsecret
+
+docker compose up -d
+
+sleep 15
+
+docker compose logs
