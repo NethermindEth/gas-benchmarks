eada823d7154d292defd1ce533869d68ea14e543
Kamil Chodoła <43241881+kamilchodola@users.noreply.github.com>
2025-06-23 14:39:12 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -96,7 +96,6 @@ for run in $(seq 1 $RUNS); do
 
     python3 -c "from utils import print_computer_specs; print(print_computer_specs())" \
     > results/computer_specs.txt
-    cat results/computer_specs.txt
 
     # Setup node (with optional image override)
     if [ -z "$IMAGES" ]; then
