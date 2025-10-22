8196d374c337bb137e1d73835498881864333126
Kamil Chodoła <kamil.chodola@gmail.com>
2025-10-22 14:09:17 +0200

    fix

--- setup_node.py
+++ setup_node.py
@@ -59,7 +59,6 @@ CLIENT_METADATA: Dict[str, Dict[str, Any]] = {
             {
                 "env": "GETH_NETWORK_FLAG",
                 "custom": "--networkid=1337",
-                "custom": "",
                 "network": lambda net: f"--{net.lower()}",
             },
             {
